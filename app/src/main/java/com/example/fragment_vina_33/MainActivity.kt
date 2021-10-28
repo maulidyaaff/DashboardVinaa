1325f19c937bd5a5cb1b5742233877f2955fcc84
maulidyaaff <vinantimaulidyafazrii@gmail.com>
2021-10-29 06:24:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragment_vina_33/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.fragment_vina_33
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.fragment_vina_33.fragment.HomeFragment
+import com.example.fragment_vina_33.fragment.NotificationsFragment
+import com.example.fragment_vina_33.fragment.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val notificationsFragment = NotificationsFragment()
+        val settingsFragment = SettingsFragment()
+
+        makeCurrentFragment (homeFragment)
+
+        val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.menu_home -> makeCurrentFragment(homeFragment)
+                R.id.menu_notifications -> makeCurrentFragment(notificationsFragment)
+                R.id.menu_settings -> makeCurrentFragment(settingsFragment)
+            }
+
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fram_nav, fragment)
+            commit()
+        }
+    }
+}
